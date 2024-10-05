d77f3423012eb5fd6d527737656c11a3db5340e6
Abstractmoney70 <99414640+Abstractmoney70@users.noreply.github.com>
2024-10-05 14:55:22 +0530

    Initial // V 1.0
    The first version of Scorching Godot, the capabilities of it are stripped down the furthest they can go, Barebones.

---
+++ app.gd
@@ -0,0 +1,72 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+func _on_item_pressed(idspecial):
+	var item_id = idspecial
+	
+	if item_id == 0:
+		$OpenFileDialog.popup()
+	elif item_id == 1:
+		$SaveFileDialog.popup()
+	elif item_id == 2:
+		get_tree().queue_free()
+	
+
+
+func _on_open_file_dialog_file_selected(path):
+	print(path)
+	var f = FileAccess.open(path, 1)
+	f.open(path, 1)
+	$CodeEdit.text = f.get_as_text()
+	f.close()
+
+func _on_save_file_dialog_file_selected(path):
+	var f = FileAccess.open(path, 2)
+	f.open(path, 2)
+	f.store_string($CodeEdit.text)
+	f.close()
+
+func _on_file_button_down() -> void:
+	$File/FileMenu.visible = true
+
+func _on_file_menu_id_pressed(id: int) -> void:
+	_on_item_pressed(id)
+	$File/FileMenu.visible = false
+
+func _helpmenu(id):
+	var item_id = id
+	
+	if item_id == 0:
+		pass
+	elif item_id == 1:
+		pass
+	elif item_id == 2:
+		pass
+	elif item_id == 3:
+		pass
+	elif item_id == 4:
+		pass
+	elif item_id == 5:
+		pass
+	elif item_id == 6:
+		pass
+	elif item_id == 7:
+		pass
+	elif item_id == 8:
+		$Help/About.visible = true
+
+func _on_help_menu_id_pressed(id: int) -> void:
+	_helpmenu(id)
+
+
+func _on_help_button_down() -> void:
+	$Help/HelpMenu.visible = true
+
+
+func _on_about_close_requested() -> void:
+	$Help/About.visible = false
---
+++ specialisedcodeedit.gd
@@ -0,0 +1,16 @@
+extends CodeEdit
+
+
+func _ready():	
+	var highlighter := CodeHighlighter.new()
+	# hi
+	highlighter.add_color_region("#", "", Color.DIM_GRAY, true)
+	highlighter.add_keyword_color("var", Color(1, 0.439, 0.518))
+	highlighter.add_keyword_color("extends", Color(1, 0.439, 0.518))
+	highlighter.add_keyword_color("func", Color(1, 0.439, 0.518))
+	highlighter.add_keyword_color("pass", Color(0.796, 0.455, 0.647))
+	highlighter.add_keyword_color("if", Color(0.796, 0.455, 0.647))
+	highlighter.add_keyword_color("elif", Color(0.796, 0.455, 0.647))
+	highlighter.add_keyword_color("else", Color(0.796, 0.455, 0.647))
+
+	syntax_highlighter = highlighter
