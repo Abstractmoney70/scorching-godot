cd4f042f19e7039bcd5d53d9051aa1ab5d27df9c
Abstractmoney70 <99414640+Abstractmoney70@users.noreply.github.com>
2024-10-05 15:46:57 +0530

    V 1.0.1.2
    Minor Patch.

--- Scripts/specialisedcodeedit.gd
+++ Scripts/specialisedcodeedit.gd
@@ -4,6 +4,16 @@ extends CodeEdit
 func _ready():	
 	var highlighter := CodeHighlighter.new()
 	# hi
+	
+	randf()
+	randomize()
+	seed(5)
+	print("oh hello world!")
+	randi_range(1, 10)
+	
+	
+	
+	
 	highlighter.add_color_region("#", "", Color.DIM_GRAY, true)
 	highlighter.add_keyword_color("var", Color(1, 0.439, 0.518))
 	highlighter.add_keyword_color("extends", Color(1, 0.439, 0.518))
@@ -13,6 +23,10 @@ func _ready():
 	highlighter.add_keyword_color("elif", Color(0.796, 0.455, 0.647))
 	highlighter.add_keyword_color("else", Color(0.796, 0.455, 0.647))
 	highlighter.set_function_color(Color(0.365, 0.804, 0.894))
-
+	highlighter.add_keyword_color("print", Color(0.553, 0.553, 0.82))
+	highlighter.add_keyword_color("seed", Color(0.553, 0.553, 0.82))
+	highlighter.add_keyword_color("randi_range", Color(0.553, 0.553, 0.82))
+	highlighter.add_keyword_color("randf", Color(0.553, 0.553, 0.82))
+	highlighter.add_keyword_color("randomize", Color(0.553, 0.553, 0.82))
 
 	syntax_highlighter = highlighter
